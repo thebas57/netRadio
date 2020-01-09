fe9e8b641b2ac543d1ef3534038b568ebe0a52a7
legrand82u <theo.legrand7@etu.univ-lorraine.fr>
2020-01-09 13:38:08 +0100

    Suppression dump inutile

---
+++ animateur/src/middlewares/EstConnectAdmin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace animateur\middlewares;
+
+use animateur\controllers\BaseController;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class EstConnectUser
+ * Acces au page seulement pour les admins
+ * @package animateur\middlewares
+ */
+class EstConnectAdmin extends BaseController
+{
+
+    /**
+     * méthode invoquée lors de l'utilisation du middleware
+     * @param $request
+     * @param $response
+     * @param $next
+     * @return ResponseInterface
+     */
+    public function __invoke($request, $response, $next)
+    {
+        if (isset($_SESSION['user'])) {
+            if ($_SESSION['user']['droit'] == 0) {
+                return $this->redirect($response, 'accueil');
+            } else {
+                $response = $next($request, $response);
+                return $response;
+            }
+        } else {
+            return $this->redirect($response, 'connexion');
+        }
+    }
+}
