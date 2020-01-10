d0319f5f6488f095882eb478157dadc6a1f75f27
legrand82u <theo.legrand7@etu.univ-lorraine.fr>
2020-01-10 14:54:34 +0100

    Correction problème Allan, c'est un caca avec Notify

--- animateur/public/js/VoirUsers.js
+++ animateur/public/js/VoirUsers.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
+    $.notify("Zizi","success");
    $(document).on('click','.supprUser',function() {
         fetch('supprUser/' + $(this).attr("data-id")).then(() => {
             $($(this).parent()).parent().remove();
+
         });
     });
 });
